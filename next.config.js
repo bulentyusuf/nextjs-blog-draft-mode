624ca9a44e79c4968a640fb915b0b3a33e54a3ab
Bulent Yusuf <bulentyusuf@fastmail.com>
2024-10-30 00:22:11 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   images: {
-    loader: "custom",
-    formats: ["image/avif", "image/webp"],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.ctfassets.net',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.eu.ctfassets.net',
+      },
+    ],
   },
 };
