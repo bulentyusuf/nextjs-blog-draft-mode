5500204cdde03ba31c2e81cef55241ecdaf5b495
Bulent Yusuf <bulentyusuf@fastmail.com>
2024-10-30 00:10:22 +0100

    Update cover-image.tsx

--- app/cover-image.tsx
+++ app/cover-image.tsx
@@ -18,8 +18,8 @@ export default function CoverImage({
     <ContentfulImage
       alt={`Cover Image for ${title}`}
       priority
-      width={1920}
-      height={1080}
+      width={2000}
+      height={1000}
       className={cn("shadow-small", {
         "hover:shadow-medium transition-shadow duration-200": slug,
       })}
