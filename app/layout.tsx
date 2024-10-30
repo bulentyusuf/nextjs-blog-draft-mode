9d9c97cf59ef1b12bbd96c29e947eec34efe62bc
Bulent Yusuf <bulentyusuf@fastmail.com>
2024-10-30 03:22:22 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,9 +52,9 @@ export default function RootLayout({
       <body>
         <section className="min-h-screen">
           <main>{children}</main>
-          <SpeedInsights />
           <Footer />
         </section>
+        <SpeedInsights />
       </body>
     </html>
   );
