9bf00a3198811c69ad65e93034394a805b2b9b76
Bulent Yusuf <bulentyusuf@fastmail.com>
2024-10-30 01:20:48 +0100

    Delete loader.js

--- my/image/loader.js
+++
@@ -1,8 +0,0 @@
-// Docs: https://www.contentful.com/developers/docs/references/images-api/
-export default function contentfulLoader({ src, width, quality }) {
-  const url = new URL(`https://images.ctfassets.net${src}`)
-  url.searchParams.set('fm', 'webp')
-  url.searchParams.set('w', width.toString())
-  url.searchParams.set('q', (quality || 75).toString())
-  return url.href
-}
