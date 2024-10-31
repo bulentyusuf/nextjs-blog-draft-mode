0b4b1cc873e7bf7efa5094a4330246f8310b4aa9
Bulent Yusuf <bulentyusuf@fastmail.com>
2024-10-31 14:43:49 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import Avatar from "./avatar";
 import MoreStories from "./more-stories";
 
 import { getAllPosts } from "@/lib/api";
-import { CMS_NAME, CMS_URL } from "@/lib/constants";
+/*import { CMS_NAME, CMS_URL } from "@/lib/constants";*/
 
 function Intro() {
   return (
@@ -15,22 +15,7 @@ function Intro() {
       <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
         Just Another Blog.
       </h1>
-      <h2 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        A statically generated blog example using{" "}
-        <a
-          href="https://nextjs.org/"
-          className="underline hover:text-success duration-200 transition-colors"
-        >
-          Next.js
-        </a>{" "}
-        and{" "}
-        <a
-          href={CMS_URL}
-          className="underline hover:text-success duration-200 transition-colors"
-        >
-          {CMS_NAME}
-        </a>
-        .
+      <h2 className="text-center md:text-left text-lg mt-5 md:pl-8">Words & Pictures made with Generative AI.
       </h2>
     </section>
   );
