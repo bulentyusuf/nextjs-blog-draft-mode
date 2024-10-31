176fcf576e91595fc0e82dbb69fbca55a1607c51
Bulent Yusuf <bulentyusuf@fastmail.com>
2024-10-31 14:44:31 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import Avatar from "./avatar";
 import MoreStories from "./more-stories";
 
 import { getAllPosts } from "@/lib/api";
-/*import { CMS_NAME, CMS_URL } from "@/lib/constants";*/
+/**import { CMS_NAME, CMS_URL } from "@/lib/constants";*/
 
 function Intro() {
   return (
