75a81e922fb37f4a4ff357d23c649a9116e11d86
Bulent Yusuf <bulentyusuf@fastmail.com>
2026-05-13 22:43:48 +0200

    fix: properly render embedded assets in rich text content

--- lib/markdown.tsx
+++ lib/markdown.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import ContentfulImage from "./contentful-image";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 import { BLOCKS } from "@contentful/rich-text-types";
 
@@ -30,11 +30,25 @@ function RichTextAsset({
 }) {
   const asset = assets?.find((asset) => asset.sys.id === id);
 
-  if (asset?.url) {
-    return <Image src={asset.url} layout="fill" alt={asset.description} />;
-  }
+  if (!asset?.url) return null;
 
-  return null;
+  return (
+    <div className="my-8">
+      <ContentfulImage
+        src={asset.url}
+        alt={asset.description || ""}
+        width={1200}
+        height={800}
+        sizes="(max-width: 768px) 100vw, 800px"
+        className="w-full h-auto"
+      />
+      {asset.description && (
+        <p className="text-sm text-gray-600 mt-2 text-center italic">
+          {asset.description}
+        </p>
+      )}
+    </div>
+  );
 }
 
 export function Markdown({ content }: { content: Content }) {
