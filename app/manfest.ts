d776087cb0e46e7d3d9924df52465995047c4c20
Bulent Yusuf <bulentyusuf@fastmail.com>
2026-05-13 22:06:45 +0200

    Create manfest.ts

---
+++ app/manfest.ts
@@ -0,0 +1,20 @@
+import { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "Fun with Gen AI",
+    short_name: "Fun with Gen AI",
+    description: "Words & Pictures made with Generative AI and a sprinkling of human thinkness.",
+    start_url: "/",
+    display: "minimal-ui",
+    background_color: "#ffffff",
+    theme_color: "#000000",
+    icons: [
+      {
+        src: "/favicon.ico",
+        sizes: "any",
+        type: "image/x-ico",
+      },
+    ],
+  };
+}
