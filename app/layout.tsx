814587b86b2e3f6a18ce0ee05a1c0d99e3a018ef
Bulent Yusuf <bulentyusuf@fastmail.com>
2026-05-13 22:13:32 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ export const metadata = {
     default: "Fun with Gen AI",
     template: "%s | Fun with Gen AI",
   },
-  description: "Words & Pictures made with Generative AI and a sprinkling of human thinkness.",
+  description: "Words & Pictures made with Generative AI and a sprinkling of Human Thinkness.",
 };
 export const viewport = {
   themeColor: "#ffffff",
