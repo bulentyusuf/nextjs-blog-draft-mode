84fbd06e3095499974c842f799e79965b64d8874
Bulent Yusuf <bulentyusuf@fastmail.com>
2026-05-21 18:24:48 +0200

    Update more-stories.tsx

--- app/more-stories.tsx
+++ app/more-stories.tsx
@@ -28,10 +28,10 @@ function PostPreview({
           sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 450px"
         />
       </div>
-      <h3 className="text-3xl mb-3 leading-snug">
-        <Link href={`/posts/${slug}`} className="hover:underline">
-          {title}
-        </Link>
+      <h3 className="text-3xl mb-3 leading-snug font-bold">
+         <Link href={`/posts/${slug}`} className="hover:text-brand-crimson transition-colors duration-200">
+            {title}
+         </Link>
       </h3>
       <div className="text-lg mb-4">
         <DateComponent dateString={date} />
