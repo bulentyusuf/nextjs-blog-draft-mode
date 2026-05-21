b0a92c1175af4ea16d798e3f79320cd56b4962e2
Bulent Yusuf <bulentyusuf@fastmail.com>
2026-05-21 19:26:10 +0200

    Update page.tsx

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -100,15 +100,14 @@ export default async function PostPage({
         type="application/ld+json"
         dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
       />
-      <article className="mx-auto max-w-4xl pt-12">
-        <div className="mb-4 text-sm text-gray-500">
-          <span>Published </span><Date dateString={post.date} />
-            {showUpdated && (
-              <span className="ml-3 whitespace-nowrap">
+      <div className="mb-4 text-sm text-gray-500">
+         <span>Published <Date dateString={post.date} /></span>
+           {showUpdated && (
+              <span className="block md:inline md:ml-3">
                  · Updated <Date dateString={post.updatedDate!} />
               </span>
-            )}
-        </div>
+           )}
+      </div>
         <h1 className="mb-6 text-5xl font-bold leading-tight tracking-tighter md:text-6xl lg:text-7xl">
           {post.title}
         </h1>
