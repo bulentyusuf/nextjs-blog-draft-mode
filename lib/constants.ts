40d0d4331032df6144c550ed5b23da2b5cfc3df8
Bulent Yusuf <bulentyusuf@fastmail.com>
2026-05-21 14:23:10 +0200

    feat: rename site to Be Useful

--- lib/constants.ts
+++ lib/constants.ts
@@ -3,10 +3,10 @@ export const SITE_URL =
 
 export const SITE_HOSTNAME = new URL(SITE_URL).hostname;
 
-export const SITE_TITLE = "Fun with GenAI";
+export const SITE_TITLE = "Be Useful";
 
 export const SITE_DESCRIPTION =
-  "Content & Code, with a little help from Generative AI.";
+  "Content & Code, with a little help from GenAI.";
 
 export const SITE_AUTHOR = "Bulent Yusuf";
 
