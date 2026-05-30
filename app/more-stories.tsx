d87dcd999c5e79b9e20e9c34971a71fad34e6ff0
Bulent Yusuf <bulentyusuf@fastmail.com>
2026-05-30 12:52:37 +0200

    Update more-stories.tsx

--- app/more-stories.tsx
+++ app/more-stories.tsx
@@ -98,7 +98,7 @@ export default function MoreStories({
 }) {
   const container =
     variant === "list"
-      ? "flex flex-col divide-y divide-gray-200 mb-12"
+      ? "flex flex-col divide-y divide-gray-200 mb-0"
       : "grid grid-cols-1 md:grid-cols-2 md:gap-x-16 lg:gap-x-32 gap-y-20 md:gap-y-32 mb-12";
 
   return (
